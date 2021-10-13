9c6a3c8e2ebf05e2ba5aaceeaa9865c0a0b3c902
Christopher Carrasco <christopher3carrasco@gmail.com>
2021-10-13 08:05:46 -0500

    Navbar y footer agregados

--- frontend/src/partials/Footer.jsx
+++ frontend/src/partials/Footer.jsx
@@ -2,7 +2,43 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <h1>Footer</h1>
+        <div class="container-fluid align-content-between py-5">
+            <div class="row">
+                <div class="col-4">
+                    <h5>AUTOTRUX</h5>
+                    <div>
+                        <img class="img-fluid w-75" src="img/AUTOTRUX_LOGO.png" alt="Logo Autotrux">
+                        <p>Gracias por visitar nuestro sitio web.</p>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <h5>MAPA DE SITIO</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Inicio</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Nosotros</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Vehículos</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Clientes</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Testimonios</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contáctanos</a></li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <h5>CONTÁCTANOS</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Dirección</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Teléfono</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Whatsapp</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Facebook</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Instagram</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Correo</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between py-4 my-4 border-top">
+                <p>Todos los derechos reservados. &copy; 2021 <a href="#">AUTOTRUX</a></p>
+                <p>Diseñado por : <a href="#">SCRUM TEAM 4</a></p>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/partials/Navbar.jsx
+++ frontend/src/partials/Navbar.jsx
@@ -2,43 +2,51 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled">Disabled</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src="img/AUTOTRUX_LOGO.png" alt width={100} height={100} />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#">Nosotros</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Vehículos
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Autos</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Camionetas</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#">Clientes</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#">Testimonios</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#">Contáctanos</a>
+                            </li>
+                        </ul>
+                        <div>
+                            <button type="button" className="btn btn-primary">Iniciar sesión</button>
+                            <button type="button" className="btn btn-info">Registrarse</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </nav>
-
+            </nav>
+        </div>
     )
 }
 
