8bc5c7f7625387076b52df5760fbaf0a98813cf5
Christopher Carrasco <christopher3carrasco@gmail.com>
2021-10-13 08:14:41 -0500

    Added navbar and footer.

--- frontend/src/partials/Navbar.jsx
+++ frontend/src/partials/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className="container">
+        <div className="container-fluid">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
